6b08c260c55e2a03ac7ce9380fc3fb84190111cc
machillix <machillix@gmail.com>
2024-08-28 15:29:42 +0300

    Add Fire Simulation Object

---
+++ Assets/Scrpts/Fire.cs
@@ -0,0 +1,67 @@
+/******************************************************************************
+* File         : Fire.cs
+* Authors      : Toni Westerlund (MaChilli/machillix)
+* Lisence      : MIT Licence
+* Copyright    : Toni Westerlund (MaChilli/machillix)
+* 
+* MIT License
+* 
+* Copyright (c) 2024 Toni Westerlund (MaChilli/machillix)
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*****************************************************************************/
+
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        SimulationManager.StartSimulation += StartFireSimulation;
+        SimulationManager.StopSimulation += StopFireSimulation;
+    }
+
+    void OnDisable()
+    {
+        SimulationManager.StartSimulation -= StartFireSimulation;
+        SimulationManager.StopSimulation -= StopFireSimulation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartFireSimulation()
+    {
+
+    }
+
+    private void StopFireSimulation()
+    {
+
+    }
+}
---
+++ Assets/Scrpts/SimulationManager.cs
@@ -0,0 +1,50 @@
+/******************************************************************************
+* File         : SimulationManager.cs
+* Authors      : Toni Westerlund (MaChilli/machillix)
+* Lisence      : MIT Licence
+* Copyright    : Toni Westerlund (MaChilli/machillix)
+* 
+* MIT License
+* 
+* Copyright (c) 2024 Toni Westerlund (MaChilli/machillix)
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*****************************************************************************/
+
+using System;
+using UnityEngine;
+
+public class SimulationManager : MonoBehaviour
+{
+
+    public static event Action StartSimulation;
+    public static event Action StopSimulation;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
